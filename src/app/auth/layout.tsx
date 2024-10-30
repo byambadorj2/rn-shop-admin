566bed2cce481df719a3a132b0fe3a64eef602d2
byambadorj2 <byambadorj993@gmail.com>
2024-10-30 19:27:32 +0800

    auth

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,6 @@
+import { ADMIN } from "@/constants/constants";
+import { createClient } from "@/supabase/server";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 export default async function AuthLayout({
@@ -5,5 +8,24 @@ export default async function AuthLayout({
 }: Readonly<{
   children: ReactNode;
 }>) {
+  const supabase = createClient();
+
+  const { data: authData } = await (await supabase).auth.getUser();
+
+  if (authData?.user) {
+    const { data, error } = await (await supabase)
+      .from("users")
+      .select("*")
+      .eq("id", authData.user.id)
+      .single();
+
+    if (error || !data) {
+      console.log("Error fetching user data", error);
+      return;
+    }
+
+    if (data.type === ADMIN) return redirect("/admin");
+  }
+
   return <>{children}</>;
 }
